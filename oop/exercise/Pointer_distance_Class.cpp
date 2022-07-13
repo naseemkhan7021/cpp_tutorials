43db204b868048197a4eb03f0d02b054c95e53fb
pyjcoder <pyjcoder.py@gmail.com>
2022-07-13 21:59:10 +0530

    add more contant

---
+++ oop/exercise/Pointer_distance_Class.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+class Point
+{
+     int x, y;
+     friend float pointDistance(Point, Point);
+
+public:
+     Point(int a, int b)
+     {
+          x = a;
+          y = b;
+     }
+
+     void DislayData(void)
+     {
+          cout << "The pointer is (" << x << ", " << y << ")" << endl;
+     }
+};
+
+// Create a funtion which takes 2 point objects and computes thedistance between those 2 points
+// d=√((x_2-x_1)²+(y_2-y_1)²)
+float pointDistance(Point a, Point b)
+{
+     return (sqrt(pow(b.x - a.x, 2) + pow(b.y-a.y, 2)));
+}
+
+int main()
+{
+     Point a(1, 1);
+     a.DislayData();
+
+     Point b(3, 5);
+     b.DislayData();
+
+     float value = pointDistance(a,b);
+     cout<<"The distance between a and b "<<value;
+
+     return 0;
+}
